6d918c60ec2240eca74c171214d7e3c64695d5fb
Rajesh Narayana Rao <mailrbn@gmail.com>
2025-05-08 09:59:51 +1000

    Last commit

---
+++ project-nine-vite/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer>
+            <p>&copy; 2024 My Website</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ project-nine-vite/src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MainContent() {
+  return (
+    <div>
+        <main>
+            <h2>Main Content</h2>
+            <p>This is a paragraph to test the webpage</p>
+        </main>
+    </div>
+  )
+}
+
+export default MainContent
